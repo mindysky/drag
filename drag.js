e3b12b1ac43175615b126838a85f6a904b8243fa
mintysky <mintyshi@163.com>
2018-01-11 09:14:25 +0800

    change master drag.js

--- drag.js
+++ drag.js
@@ -2,8 +2,11 @@ function (id) {
     var obj = document.getElementsById(id);
     var disX = 0;
     var disY = 0;
-    var h =0;
-    obj.onmousedown = function(){
+    obj.onmousedown = function(ev){
+        var disX = ev.pageX - obj.offsetLeft;
+        var disY = ev.pageY - obj.offsetTop;
+        obj.onmousemove = function(){
 
+        };
     };
 }
