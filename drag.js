6345e37d28bc10dee728fbf809b6c0d3c59b2932
mintysky <mintyshi@163.com>
2018-01-11 09:16:24 +0800

    change sky1 drag.js

--- drag.js
+++ drag.js
@@ -2,8 +2,9 @@ function (id) {
     var obj = document.getElementsById(id);
     var disX = 0;
     var disY = 0;
-    var h =0;
     obj.onmousedown = function(){
-
+        var objL = ev.pageX - obj.offsetX;
+        var objT = ev.pageY - obj.offsetY;
+        
     };
 }
